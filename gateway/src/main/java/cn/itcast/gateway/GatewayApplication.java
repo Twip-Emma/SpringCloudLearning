57051f27c909b7b548c666a2bc864d0bfb8a6148
七画一只妖 <1157529280@qq.com>
2022-01-11 14:56:28 +0800

    使用网关，并且创建本地过滤器，路由过滤器，全局过滤器，并且比较之间的异同，作用，以及优先顺序；解决跨域

---
+++ gateway/src/main/java/cn/itcast/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package cn.itcast.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: 七画一只妖
+ * @Date: 2022/1/11 12:45
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
