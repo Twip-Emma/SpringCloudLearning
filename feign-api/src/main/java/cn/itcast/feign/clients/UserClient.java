526769b98cfcb654f3a7d9a7620e7b38719fcd9c
七画一只妖 <1157529280@qq.com>
2022-02-09 10:56:12 +0800

    整合FeignClient，服务降级，服务熔断

--- feign-api/src/main/java/cn/itcast/feign/clients/UserClient.java
+++ feign-api/src/main/java/cn/itcast/feign/clients/UserClient.java
@@ -1,5 +1,6 @@
 package cn.itcast.feign.clients;
 
+import cn.itcast.feign.clients.fallback.UserClientFallbackFactory;
 import cn.itcast.feign.pojo.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @Author: 七画一只妖
  * @Date: 2022/1/11 11:04
  */
-@FeignClient("userservice")
+@FeignClient(value = "userservice",fallbackFactory = UserClientFallbackFactory.class)
 public interface UserClient {
     @GetMapping("/user/{id}")
     User findById(@PathVariable("id") Long id);
--- user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -28,12 +28,12 @@ public class UserController {
     private PatternProperties patternProperties;
 
     @GetMapping("prop")
-    public PatternProperties patternProperties(){
+    public PatternProperties patternProperties() {
         return patternProperties;
     }
 
     @GetMapping("now")
-    public String now(){
+    public String now() {
         return LocalDateTime.now().format(DateTimeFormatter.ofPattern(patternProperties.getDateformat()));
     }
 
@@ -45,8 +45,14 @@ public class UserController {
      */
     @GetMapping("/{id}")
     public User queryById(@PathVariable("id") Long id,
-                          @RequestHeader(value = "Truth", required = false) String truth) {
+                          @RequestHeader(value = "Truth", required = false) String truth) throws InterruptedException {
+        if (id == 1) {
+            Thread.sleep(60);
+        } else if (id == 2) {
+            throw new RuntimeException("爷故意的！");
+        }
         System.out.println("truth: " + truth);
+
         return userService.queryById(id);
     }
 }
